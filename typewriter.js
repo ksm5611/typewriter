0fe352d98715872ce6d08613bfcfc0950c471907
ksm5611 <kim.sumin.wat2014@gmail.com>
2021-05-04 22:16:09 +0000

    changed

--- typewriter.js
+++ typewriter.js
@@ -1,10 +1,14 @@
-const sentence = "hello there from lighthouse labs";
+const sentence = "hello there from lighthouse labs\n";
 
-setTimeout(() => {
-  // print the char here
-  for (const char of sentence) {
-    // console.log(char);
-    process.stdout.write(char); //avoid automatically adding an extra "newline character" at the end each time.
-  }
-  console.log('\n');
-}, 1000); // <= 1s delay to make it noticeable. Can dial it down later.
+let start = 0;
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, start);
+  start += 50;
+}
+
+
+//     process.stdout.write(char); //avoid automatically adding an extra "newline character" at the end each time.
+
+
